7ed063cc72056e93e47aa81fcac75dc1c137636b
Charlie Todd (zerolagtime) <zerolagtime@gmail.com>
2024-09-22 22:46:23 -0400

    overhaul to use AL2023 and add cloudwatch with metrics

--- up.sh
+++ up.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+# Copyright 2024 zerolagtime[at]gmail[dot]com
+# See the LICENSE.txt for additional rights
+#
+# Stand up a small AWS EC2 instance that runs WireGuard,
+# and then places the client configuration file in 
+# the local folder for use by a desktop. Effectively,
+# all traffic for a node is routed through AWS.
+
+here=$(dirname "$0")
+if [[ $here == "." ]]; then
+   here=${PWD}
+fi
+function is_valid_json() {
+   input_file=${1}
+   if output=$(jq . ${input_file}); then
+      /bin/true
+   else
+      echo "ERROR: $input_file is invalid json.  Stopping."
+      echo "$output"
+      exit 1
+   fi
+}
 
 function is_deployed() {
    stack=$1
@@ -13,13 +35,13 @@ function wait_for_stack() {
    stack=$1
    verb=${2:-CREATE}
    status="PENDING"
-   until [ "$status" == "${verb}_COMPLETE" -o "$status" == "FAILURE" ]; 
+   until [ "$status" == "${verb}_COMPLETE" -o "$status" == "FAILURE" -o "$status" == "ROLLBACK_COMPLETE" ]; 
    do
      status=$(aws cloudformation describe-stacks --stack-name $stack | jq --arg stack $stack -r '.Stacks[] | select(.StackName==$stack) | .StackStatus')
      if [ -z "$status" ]; then
         status="MISSING"
      fi
-     if [ "$status" != "${verb}_COMPLETE" ]; then 
+     if [ "$status" != "${verb}_COMPLETE" -a "$status" != "ROLLBACK_COMPLETE" ]; then 
         echo "[$(date)] [INFO] [$stack] waiting for stack to come up.  Currently $status"
         sleep 5
      fi
@@ -36,12 +58,14 @@ function die() {
    echo "[$(date)] [FATAL] $msg" 1>&2 
    exit 1
 }
+is_valid_json "$here/wireguard-eip-master.json"
+is_valid_json "$here/wireguard-eip-master.json"
 output=$(is_deployed wg1-eip)
 if [ $? -eq 1 ]; then
-   aws cloudformation create-stack --stack-name wg1-eip --template-body file://wireguard-eip-master.json || die "stack wg1-eip could not deploy"
+   aws cloudformation create-stack --stack-name wg1-eip --template-body file://${here}/wireguard-eip-master.json || die "stack wg1-eip could not deploy"
    wait_for_stack wg1-eip  || die "Stack wg1-eip failed to deploy"
 else
-   aws cloudformation update-stack --stack-name wg1-eip --template-body file://wireguard-eip-master.json 
+   aws cloudformation update-stack --stack-name wg1-eip --template-body file://${here}/wireguard-eip-master.json 
    err=$?
    if [ $err -eq 0 ]; then
       wait_for_stack wg1-eip UPDATE || die "Stack wg1-eip failed to update"
@@ -54,7 +78,7 @@ fi
 get_config=0
 default_sg=$(aws ec2 describe-security-groups | jq -r '.SecurityGroups[] | select(.GroupName=="default") | .GroupId ' | head -1)
 if is_deployed wg1 ; then
-   aws cloudformation update-stack --stack-name wg1 --template-body file://wireguard-master.json --parameters ParameterKey=VpnSecurityGroupID,ParameterValue=$default_sg --capabilities CAPABILITY_IAM
+   aws cloudformation update-stack --stack-name wg1 --template-body file://${here}/wireguard-master.json --parameters ParameterKey=VpnSecurityGroupID,ParameterValue=$default_sg --capabilities CAPABILITY_IAM
    err=$?
    if [ $err -eq 0 ]; then
       wait_for_stack wg1 UPDATE || (die "Stack wg1 failed to update")
@@ -65,12 +89,13 @@ if is_deployed wg1 ; then
    fi
    wait_for_stack wg1 UPDATE || die "Stack wg1 failed to update"
 else
-   aws cloudformation create-stack --stack-name wg1 --template-body file://wireguard-master.json --parameters ParameterKey=VpnSecurityGroupID,ParameterValue=$default_sg --capabilities CAPABILITY_IAM
-   wait_for_stack wg1 || (./down.sh; die "Stack wg1 did not come up")
+   aws cloudformation create-stack --stack-name wg1 --template-body file://${here}/wireguard-master.json --parameters ParameterKey=VpnSecurityGroupID,ParameterValue=$default_sg --capabilities CAPABILITY_IAM
+   wait_for_stack wg1 || ($here/down.sh; die "Stack wg1 did not come up")
    get_config=1
 fi
 if [ $get_config -eq 1 ]; then
-   conf=aws_$(date +%Y%m%d).conf
+   # allow for up.sh to be called in a folder away from where the key is stored
+   conf=${PWD}/aws_$(date +%Y%m%d).conf
    triesLeft=25
    delays=15
    while [ $triesLeft -gt 0 ]; do
@@ -93,5 +118,6 @@ if [ $get_config -eq 1 ]; then
       echo "[$(date)] [WARN] Config parameter not downloaded and so not deleted."
    fi
 else
+   mv $conf{,.failed}
    echo "[$(date)] [INFO] Wireguard config not updated.  Use last known configuration file."
 fi
